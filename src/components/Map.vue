7cb9288f9d14e0be306405b8f5571c6e800dce50
attotem <vitalik-ama@proton.me>
2025-11-17 14:10:58 +0100

    dnipro on mao

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -98,13 +98,20 @@ function onMarkerClick(center) {
 function closePopup() {
 	activePopup.value = null
 }
-
-watch(() => useLangStore().activeLang, async () => {
-  const coordinatesMap = {
+const coordinatesMap = {
 		"ae039f08-23cb-4384-814c-dbab8d54417d": { lat: 50.473385, lng: 30.518172, name: 'Автосалон+Електромобілів+«SKM-1»', link: 'https://www.google.com/maps/place/%D0%90%D0%B2%D1%82%D0%BE%D1%81%D0%B0%D0%BB%D0%BE%D0%BD+%D0%95%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BC%D0%BE%D0%B1%D1%96%D0%BB%D1%96%D0%B2+%C2%ABSKM-1%C2%BB/@50.4732628,30.515803,17z/data=!3m1!4b1!4m6!3m5!1s0x40d4cf6b50a498f1:0xef1666fc9b9b1d8!8m2!3d50.4732594!4d30.5183779!16s%2Fg%2F11w918zsyj?entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D' },
 		"25af8dc3-9b17-4547-be4d-48de4c6c3ef4": { lat: 50.396877, lng: 30.620526, name: 'Автосалон+Zeekr+Позняки', link: 'https://www.google.com/maps/place/%D0%90%D0%B2%D1%82%D0%BE%D1%81%D0%B0%D0%BB%D0%BE%D0%BD+Zeekr+%D0%9F%D0%BE%D0%B7%D0%BD%D1%8F%D0%BA%D0%B8/@50.3969181,30.6179639,17z/data=!3m1!4b1!4m6!3m5!1s0x40d4c583ea7c3ef1:0x18f3fe9250161f11!8m2!3d50.3969147!4d30.6205388!16s%2Fg%2F11x7pn58jv?entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D' },
-		"ac901e17-73b9-4b7e-a25f-1564ee2b103e": { lat: 46.418262, lng: 30.725243, name: 'Автосалон+Zeekr+Одеса', link: 'https://www.google.com/maps/place/%D0%90%D0%B2%D1%82%D0%BE%D1%81%D0%B0%D0%BB%D0%BE%D0%BD+Zeekr+%D0%9E%D0%B4%D0%B5%D1%81%D0%B0/@46.4181256,30.7226968,17z/data=!3m1!4b1!4m6!3m5!1s0x40c6333bd4933525:0xd7a6bb1665ccbe60!8m2!3d46.4181219!4d30.7252717!16s%2Fg%2F11m759cd9l?entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D' }
+		"ac901e17-73b9-4b7e-a25f-1564ee2b103e": { lat: 46.418262, lng: 30.725243, name: 'Автосалон+Zeekr+Одеса', link: 'https://www.google.com/maps/place/%D0%90%D0%B2%D1%82%D0%BE%D1%81%D0%B0%D0%BB%D0%BE%D0%BD+Zeekr+%D0%9E%D0%B4%D0%B5%D1%81%D0%B0/@46.4181256,30.7226968,17z/data=!3m1!4b1!4m6!3m5!1s0x40c6333bd4933525:0xd7a6bb1665ccbe60!8m2!3d46.4181219!4d30.7252717!16s%2Fg%2F11m759cd9l?entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D' },
+		"ced6fe8c-aadc-4b39-9b0d-357bd09e4da8": { 
+			lat: 48.3957374, 
+			lng: 35.043929, 
+			name: 'Автосалон+Zeekr+Дніпро', 
+			link: 'https://maps.app.goo.gl/e6zKAFixSckortbp7' 
+		}
+
 		};
+watch(() => useLangStore().activeLang, async () => {
+
 	let contactsData = await API.ContactsPage.get();
 
 	if (contactsData.center_contacts?.length) {
@@ -132,11 +139,7 @@ watch(() => useLangStore().activeLang, async () => {
 })
 
 onMounted(async () => {
-	const coordinatesMap = {
-		"ae039f08-23cb-4384-814c-dbab8d54417d": { lat: 50.473385, lng: 30.518172, name: 'Автосалон+Електромобілів+«SKM-1»', link: 'https://www.google.com/maps/place/%D0%90%D0%B2%D1%82%D0%BE%D1%81%D0%B0%D0%BB%D0%BE%D0%BD+%D0%95%D0%BB%D0%B5%D0%BA%D1%82%D1%80%D0%BE%D0%BC%D0%BE%D0%B1%D1%96%D0%BB%D1%96%D0%B2+%C2%ABSKM-1%C2%BB/@50.4732628,30.515803,17z/data=!3m1!4b1!4m6!3m5!1s0x40d4cf6b50a498f1:0xef1666fc9b9b1d8!8m2!3d50.4732594!4d30.5183779!16s%2Fg%2F11w918zsyj?entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D' },
-		"25af8dc3-9b17-4547-be4d-48de4c6c3ef4": { lat: 50.396877, lng: 30.620526, name: 'Автосалон+Zeekr+Позняки', link: 'https://www.google.com/maps/place/%D0%90%D0%B2%D1%82%D0%BE%D1%81%D0%B0%D0%BB%D0%BE%D0%BD+Zeekr+%D0%9F%D0%BE%D0%B7%D0%BD%D1%8F%D0%BA%D0%B8/@50.3969181,30.6179639,17z/data=!3m1!4b1!4m6!3m5!1s0x40d4c583ea7c3ef1:0x18f3fe9250161f11!8m2!3d50.3969147!4d30.6205388!16s%2Fg%2F11x7pn58jv?entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D' },
-		"ac901e17-73b9-4b7e-a25f-1564ee2b103e": { lat: 46.418262, lng: 30.725243, name: 'Автосалон+Zeekr+Одеса', link: 'https://www.google.com/maps/place/%D0%90%D0%B2%D1%82%D0%BE%D1%81%D0%B0%D0%BB%D0%BE%D0%BD+Zeekr+%D0%9E%D0%B4%D0%B5%D1%81%D0%B0/@46.4181256,30.7226968,17z/data=!3m1!4b1!4m6!3m5!1s0x40c6333bd4933525:0xd7a6bb1665ccbe60!8m2!3d46.4181219!4d30.7252717!16s%2Fg%2F11m759cd9l?entry=ttu&g_ep=EgoyMDI1MDQyNy4xIKXMDSoASAFQAw%3D%3D' }
-		};
+	
 	let contactsData = await API.ContactsPage.get();
 
 	if (contactsData.center_contacts?.length) {
